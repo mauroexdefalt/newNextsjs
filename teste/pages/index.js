3ae296a84dae3eddda42eb8f6440d2c39ca82a37
Mauro <exdefalt951@gmail.com>
2021-05-12 21:11:01 -0300

    teste

--- teste/pages/index.js
+++ teste/pages/index.js
@@ -1,41 +1,52 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
 
 
-  const [teste, setTeste] = ['https://cdn.pixabay.com/photo/2019/04/04/15/17/smartphone-4103051__340.jpg',
-  'https://cdn.pixabay.com/photo/2013/12/11/03/13/puzzle-226743__340.jpg',
-
-]
-
-    useEffect(() => {
 
 
+  const [pos, setPos] = useState(0)
 
-    }, [])
+  const imgs = ['https://cdn.pixabay.com/photo/2019/04/04/15/17/smartphone-4103051__340.jpg',
+    'https://cdn.pixabay.com/photo/2013/12/11/03/13/puzzle-226743__340.jpg',
+    "https://ichi.pro/assets/images/max/724/1*Hva7hcsFWulFUPhrEWui1A.jpeg"
+
+  ]
+
+
 
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <meta name="description" content="teste nextjs" />
-           <meta property="og:image" content="https://ichi.pro/assets/images/max/724/1*Hva7hcsFWulFUPhrEWui1A.jpeg" />
+        <meta property="og:image" content={imgs[pos]} />
       </Head>
 
 
 
-      <button onClick={()=>{
+      <button onClick={() => {
+        setPos(pos + 1)
+        console.log(pos  , imgs[pos])
 
-      }} >muda head</button>
-
-<Image src="/assets/fox.jpg" alt="Vercel Logo" width={300} height={300} />
-
-     
+      }} >next</button>
 
       
+<button onClick={() => {
+        setPos(pos - 1)
+        console.log(pos , imgs[pos])
+
+      }} >back</button>
+
+
+      <Image src="/assets/fox.jpg" alt="Vercel Logo" width={300} height={300} />
+
+
+
+
     </div>
   )
 }
